3e5f03eaab53ef96ee114e449f33fc90a92325dd
Tanner Langlinais <tlanglinais@gmail.com>
2019-10-23 08:34:37 -0500

    Implemented geocoder

--- server.js
+++ server.js
@@ -1,14 +1,43 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+const colors = require("colors");
+// const errorHandler = require("./middleware/error");
+const connectDB = require("./config/db");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
 
+// Connect to database
+connectDB();
+
 const app = express();
+
+// Body parser
+app.use(express.json());
+
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+// Mount routers
+app.use("/api/v1/bootcamps", require("./routes/bootcamps.js"));
+
+app.use(require("./middleware/error"));
+
+const server = app.listen(
   PORT,
-  console.log(`Server started in ${process.env.NODE_ENV} mode on port ${5000}`)
+  console.log(
+    `Server started in ${process.env.NODE_ENV} mode on port ${5000}`.yellow.bold
+  )
 );
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server and exit process
+  server.close(() => process.exit(1));
+});
